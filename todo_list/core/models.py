47a062e6ad01e298c9f9846791e646318dbaac66
Joseph Boscan <joeypc.py@gmail.com>
2021-09-18 21:41:58 -0400

    Creating the first view and making migrations

--- todo_list/core/models.py
+++ todo_list/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Task(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=255, blank=False)
+    description = models.TextField()
+    complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('complete',)
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
