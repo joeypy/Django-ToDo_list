32e359b3207b6e199ee0f283a50534ab113936da
Joseph Boscan <joeypc.py@gmail.com>
2021-09-18 21:26:27 -0400

    App core created, register the app in settings.py

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -20,6 +20,8 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -42,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Django apps
+    'core.apps.CoreConfig'
 ]
 
 MIDDLEWARE = [
